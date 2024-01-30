8153072b11b3551f35f58141d59b03d8992e69a5
Aryan-Diwakar-1856 <diwakerryan7900@gmail.com>
2024-01-31 00:31:06 +0530

    30-01-2024: Both tasks done

---
+++ POTD/Week 5-6 (Binary Search)/30-01-2024/1st.java
@@ -0,0 +1,58 @@
+/**
+ * Link - https://www.geeksforgeeks.org/problems/number-of-occurrence2259/1
+ * Number of occurrence
+MediumAccuracy: 59.34%Submissions: 176K+Points: 4
+30+ People have Claimed their 90% Refunds. Start Your Journey Today! 
+
+banner
+Given a sorted array Arr of size N and a number X, you need to find the number of occurrences of X in Arr.
+
+Example 1:
+
+Input:
+N = 7, X = 2
+Arr[] = {1, 1, 2, 2, 2, 2, 3}
+Output: 4
+Explanation: 2 occurs 4 times in the
+given array.
+Example 2:
+
+Input:
+N = 7, X = 4
+Arr[] = {1, 1, 2, 2, 2, 2, 3}
+Output: 0
+Explanation: 4 is not present in the
+given array.
+Your Task:
+You don't need to read input or print anything.
+Your task is to complete the function count() which takes the array of integers arr, n, and x as parameters and returns an integer denoting the answer.
+If x is not present in the array (arr) then return 0.
+
+Expected Time Complexity: O(logN)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ N ≤ 105
+1 ≤ Arr[i] ≤ 106
+1 ≤ X ≤ 106
+
+
+ */
+
+class Solution {
+    int count(int[] arr, int n, int x) {
+        // code here
+        int l=0;
+        int r=n-1;
+        
+        while(l<=r){
+            if(arr[l]==x && arr[r]==x)
+                break;
+            if(arr[l]!=x)
+                l++;
+            if(arr[r]!=x)
+                r--;
+        }
+        return (r>=l)?r-l+1:0;
+    }
+}
